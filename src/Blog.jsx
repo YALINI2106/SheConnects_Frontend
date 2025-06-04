48b6afc273a20b481cde9747ebff7f3b752e790e
YALINI2106 <153434405+YALINI2106@users.noreply.github.com>
2025-06-04 20:26:59 +0530

    Update Blog.jsx

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Blog.css";
+import "./blog.css";
 
 const Blog = () => {
   const [title, setTitle] = useState("");
